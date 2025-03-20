6900332cf9173eb93ca24c34bc4b0edf2408a452
ParkKyunglim <pkl1004125@gmail.com>
2025-03-21 00:21:23 +0900

    add json

---
+++ json.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// JSON (JavaScript Object Notation)
+// 데이터를 저장하고 전송할 때 사용되는 가볍고 구조적인 데이터 포맷
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${name} can jump!`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value; // name을 'ellie'로 변경
+});
+console.log(json);
+
+
+// 2. JSON to Object
+// JSON.parse(json) : JSON 문자열을 객체로 변환
+
+console.clear(); 
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
